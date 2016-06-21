2e798ea617cbc394542fc57e08a8eca5e1e95331
joschi127 <127.joschi@gmail.com>
2016-06-21 13:48:40 +0200

    Removed dotdeb cookbook, added own dotdeb code

--- site-cookbooks/app/recipes/web_server.rb
+++ site-cookbooks/app/recipes/web_server.rb
@@ -20,7 +20,7 @@ end
 # Install Apache
 include_recipe "openssl"
 include_recipe "apache2"
-include_recipe "apache2::mod_php5"
+#include_recipe "apache2::mod_php5"
 include_recipe "apache2::mod_rewrite"
 include_recipe "apache2::mod_ssl"
 include_recipe "apache2::mod_proxy"
