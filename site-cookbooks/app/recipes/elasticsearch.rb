45f3389a80462a1eaf6347c98363761554c67c0b
Janosch Rolles <janosch.rolles@seitwerk.de>
2018-05-07 11:52:30 +0200

    Fix elasticsearch install

--- site-cookbooks/app/recipes/elasticsearch.rb
+++ site-cookbooks/app/recipes/elasticsearch.rb
@@ -17,6 +17,9 @@ end
 elasticsearch_install 'my_es_installation' do
   type 'tarball' # type of install
   version '1.7.2'
+  download_url "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz"
+  # sha256
+  download_checksum "6f81935e270c403681e120ec4395c28b2ddc87e659ff7784608b86beb5223dd2"
   action :install # could be :remove as well
 end
 
